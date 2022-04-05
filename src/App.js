71b997c6a934ed5df9a63aff3522646a0de7a62c
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-06 03:31:07 +0600

    useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 
@@ -7,6 +7,18 @@ import './App.css';
 const API_URL = 'http://www.omdbapi.com?apikey=a244e5b6';
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
   return (
     <h1>
       Hello
